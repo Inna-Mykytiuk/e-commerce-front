a9982a87cc1b0de78cd09a228514cac3054ffdf4
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-09-02 12:31:23 +0300

    create shopping-view structure

---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>page doesn&apos;t exist</div>;
+}
+
+export default NotFound;
---
+++ src/pages/unauth-page/index.jsx
@@ -0,0 +1,5 @@
+function UnauthPage() {
+  return <h1>You don&apos; t have access to view this page</h1>;
+}
+
+export default UnauthPage;
